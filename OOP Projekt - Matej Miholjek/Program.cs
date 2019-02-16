b01efbea42c374cbae95b80111d0ef75d58d24e8
mmiholjek <mmiholjek@yahoo.com>
2019-02-16 09:09:54 +0100

    Add project files.

---
+++ OOP Projekt - Matej Miholjek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Projekt___Matej_Miholjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
